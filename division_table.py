25199d0fb958070967f45a519947de5475e9b6be
your favorite edgelord™ <35822767+satanmyninjas@users.noreply.github.com>
2019-10-23 18:48:32 -0400

    Update division_table.py

--- division_table.py
+++ division_table.py
@@ -1,4 +1,4 @@
-user_input = int(input('Enter the number you\'d like a division table for. \n'))
+user_input = int(input('Enter the number you\'d like a division table for.\n'))
 
-for _ in range(1, user_input + 1):
-    print(user_input, '/', _, '=', user_input / _)
+for _ in range(1, user_input+1):
+    print(user_input, '/', _, '=', user_input/_)
