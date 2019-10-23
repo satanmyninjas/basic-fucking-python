0b82aa7c933e39d127fe5046c202d316d34e5486
your favorite edgelord™ <35822767+satanmyninjas@users.noreply.github.com>
2019-10-23 18:47:37 -0400

    Update mutliplication_table.py

--- mutliplication_table.py
+++ mutliplication_table.py
@@ -1,4 +1,4 @@
-user_input = int(input('Enter enter the number you\'d like a times table for. \n'))
+user_input = int(input('Enter the number you\'d like a times table for. \n'))
 
 for _ in range(1, user_input+1):
     print(user_input, 'x', _, '=', user_input*_)
