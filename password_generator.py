690ece3f7ae1037fa85c459f2cb12c9b6d879376
satanmyninjas <kaylafernandez55@gmail.com>
2019-11-03 20:17:04 -0500

    yeet

--- password_generator.py
+++ password_generator.py
@@ -9,7 +9,6 @@ amount_of_passwords = int(input(f'Enter the number of passwords you '
                                 f'would like to create: '))
 length = int(input("Enter the desired length for your password: "))
 
-
 for amount in range(amount_of_passwords):
     password = ''
     for chars in range(length):
