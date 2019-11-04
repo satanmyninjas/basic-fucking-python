5a9afd3b36c240fc900048c9a549c0be7aa52ec2
satanmyninjas <kaylafernandez55@gmail.com>
2019-11-03 20:06:57 -0500

    add some shit

--- math_shit/triangle_checker.py
+++ math_shit/triangle_checker.py
@@ -2,7 +2,6 @@ def is_triangle(a, b, c):
 
     ordered_sides = [a, b, c]
     ordered_sides.sort()
-
     a = ordered_sides[0]
     b = ordered_sides[1]
     c = ordered_sides[2]
---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+
+
+characters = 'abcdefghijklmnopqrstuvwyxz' \
+             'ABCDEFGHIJKLMNOPQRSTUVWYXZ' \
+             '1234567890`~-_=+,<.>/?;:{[]}|'
+
+amount_of_passwords = int(input(f'Enter the number of passwords you '
+                                f'would like to create: '))
+length = int(input("Enter the desired length for your password: "))
+
+
+for amount in range(amount_of_passwords):
+    password = ''
+    for chars in range(length):
+        password += random.choice(characters)
+    print(password)
